7949768e798907859ebf008aef7cd4a2b48e5486
unknown <redlos2712@gmail.com>
2021-09-18 12:17:41 +0200

    init repo

--- README.md
+++ README.md
@@ -1,2 +1,45 @@
 # royalroad-fetchAndConvert
 Automatically download a royalroad story, convert it to kindle format and push it to the device.
+
+
+# Features
+
+* Retrieve automatically all the chapters of the given story
+* Support table of content for ebook conversion
+* Support author note at both end and start of the chapter
+* Support images
+* Support CSS rules for different stories (e.g. "Everybody Love Large Chest")
+* Very low memory and performance footprint
+* Convert the story in the azw3 format, and push it to the kindle.
+
+
+# Libraries used
+
+This script uses requests_html python package. https://html.python-requests.org/. Install it :  
+
+	pip install requests
+
+This script is based on this royalroad fetcher : [royalroad-reader](https://github.com/lpicou/royalroad-reader/). It also uses [calibre](https://github.com/kovidgoyal/calibre) to convert and push the ebook to the kindle. Everything is included here. 
+
+# Usage 
+
+	fetchAndCopy.py url_of_chapter number_of_chapters name_of_ebook
+
+Parameters :  
+
+* url_of_chapter : The URL of the chapter (not of the story homepage!)
+* number_of_chapters : You guessed. If you want the whole book, just enter something like 99999
+* name_of_ebook : The name of the ebook, .html extension will be added when the file is written on the filesystem. 
+
+It reads the chapter content of the URL given ( url_of_chapter ) and goes to the next chapter, and it loops number_of_chapters times.  
+name_of_ebook is the name of the ebook which will be pushed to the kindle.  
+
+This script write .html and .azw3 file temporary on the filesystem, and then delete them.
+
+## Example  
+
+If you want to fetch the best rated story Mother of Learning from the first chapter, execute the following command :
+
+    python fetch_book.py https://www.royalroad.com/fiction/21220/mother-of-learning/chapter/301778/1-good-morning-brother 102 MOL
+  
+In the example above, MOL.html is written a the working directory.
---
+++ fetch_book.py
@@ -0,0 +1,115 @@
+from requests_html import HTMLSession
+import sys
+import time
+
+#GNU GPLv3 License https://www.gnu.org/licenses/gpl-3.0.en.html
+
+
+
+
+def main(next_url,nb_chapters,file_name,writeFile=True):
+
+    CSS='''<style>
+    .chapter-content table {
+        background: #004b7a;
+        margin: 10px auto;
+        width: 90%;
+        border: none;
+        box-shadow: 1px 1px 1px rgba(0,0,0,.75);
+        border-collapse: separate;
+        border-spacing: 2px;
+    }
+    .chapter-content table td {
+        color: #ccc;
+    }
+    </style>
+    '''
+
+    print("URL : ",next_url)
+    print("Number of chapters : ",nb_chapters)
+    print("File name : ",file_name,"\n")
+
+    session = HTMLSession()
+    if writeFile:
+        f = open(file_name,"w+",encoding="utf-8")
+        f.write(CSS)
+
+    book = CSS
+    for i in range(1,nb_chapters+1):
+        try:
+            
+            s = session.get(next_url)
+            S=""
+            
+            # Website implemented protection against scraping
+            # "Slow down!"
+            while(s.status_code == 429):
+                time.sleep(0.2)
+                s = session.get(next_url)
+
+            #Fetch title
+            chapter_title = (s.html.find('h1.font-white')[0]).text
+            S+='<h1 class=\"chapter\">' + chapter_title + '</h1>\n'
+
+            print(i," ",chapter_title)
+            
+            #Fetch the chapter content
+            chapter_content = s.html.find('.chapter-inner',first=True).html
+            S+=chapter_content
+            
+            #Fetch the author note (if there is one)
+            author_note = s.html.find('.portlet-body.author-note')
+            if(len(author_note)==1): 
+                note_content = author_note[0].html
+                S+='<h3 class=\"author_note\"> Author note </h3>\n'
+                S+=note_content
+            elif(len(author_note)==2):
+                note_content = author_note[0].html
+                S+='<h3 class=\"author_note\"> Author note top page </h3>\n'
+                S+=note_content
+                note_content = author_note[1].html
+                S+='<h3 class=\"author_note\"> Author note bottom page </h3>\n'
+                S+=note_content
+            
+            book += S
+
+            if writeFile:
+                #Write the whole content in the file
+                f.write(S)
+
+            try:
+                #Fetch the url of the next chapter
+                next_url = "https://www.royalroad.com" + (s.html.find('[rel=next]')[0]).attrs.get("href")
+            except:
+                print("Last chapter ! Exiting...")
+                break
+            
+        except:
+            print("Error on chapter",i,sys.exc_info()[0])
+
+            if writeFile:
+                f.close()
+
+            break
+    
+    f.close()    
+    return S
+        
+    
+
+
+def printUsage():
+    print("Usage :\n   fetch_book.py url_of_chapter number_of_chapters name_of_ebook")
+
+
+if __name__ == "__main__":
+    args = sys.argv[1:]
+    if(len(args) != 3):
+        printUsage()
+        exit()
+
+    next_url = args[0]
+    nb_chapters = int(args[1])
+    file_name = args[2] +".html"
+    main(next_url,nb_chapters,file_name)
+    exit()
